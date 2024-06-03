8180a55f944bd80f76c7e3b8d56a9edf81a174e7
michalmoninec <57136223+michalmoninec@users.noreply.github.com>
2024-06-03 07:53:51 +0200

    rough working version
    works, but needs refactoring and tests

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import shutil
 from pathlib import Path
 import hashlib
 import argparse
+from typing import List
 import schedule
 import logging
 from datetime import datetime
@@ -13,16 +14,17 @@ def get_valid_abs_path(argument: str) -> Path:
     if os.path.exists(Path(argument).absolute()):
         return Path(argument).absolute()
     else:
-        raise argparse.ArgumentTypeError("Directory does't exist. Provide existing directory, or create requested directory.")
+        raise argparse.ArgumentTypeError(
+            "Directory does't exist. \n"
+            "Provide existing directory, or create requested directory."
+        )
 
-def get_valid_integer(argument: str) -> int:
+def get_valid_interval(argument: str) -> int:
     if int(argument)>0:
         return int(argument)
     else:
         raise argparse.ArgumentTypeError("Sync interval is not greater than 0")
 
-#split into seperate functions
-#use decorator for logging
 def handle_file(mode: str, src_root_path, rep_root_path, **kwargs) -> None:
     if 's_rel' in kwargs:
         src = Path(src_root_path,kwargs['s_rel'])
@@ -46,7 +48,6 @@ def handle_file(mode: str, src_root_path, rep_root_path, **kwargs) -> None:
         txt = 'REMOVED FILE: '
         logging.info(f"{txt:<15} {rm_dst}")
 
-#mode handle as an enumerate
 def handle_dir(mode: str, rep_root_path, rel_path) -> None:
     dst = Path(rep_root_path, rel_path)
     if mode == 'create':
@@ -60,7 +61,7 @@ def handle_dir(mode: str, rep_root_path, rel_path) -> None:
 
 
 
-def get_files_path_list(absolute_path: Path) -> list:
+def get_files_path_list(absolute_path: Path) -> List[Path]:
     file_list = []
     dir_list = []
     for path, subdirs, files in os.walk(absolute_path, topdown=False):
@@ -85,7 +86,8 @@ def included_in_list_parents(dir_path: Path, dir_list: list) -> bool:
 def files_have_same_hash(fl1_path: Path, fl2_path: Path) -> bool:
     with open(fl1_path, 'rb') as fl1:
         with open(fl2_path, 'rb') as fl2:
-            if hashlib.md5(fl1.read()).hexdigest() == hashlib.md5(fl2.read()).hexdigest():
+            if hashlib.md5(
+                fl1.read()).hexdigest() == hashlib.md5(fl2.read()).hexdigest():
                 return True
             else:
                 return False
@@ -94,6 +96,7 @@ def sync_folders(src_root_path: Path, rep_root_path: Path) -> None:
     src_files_path_list, src_dirs_path_list = get_files_path_list(src_root_path)
     rep_files_path_list, rep_dirs_path_list = get_files_path_list(rep_root_path)
 
+    #separate function
     for src_dir_path in src_dirs_path_list:
         if src_dir_path not in rep_dirs_path_list:
             if not os.path.exists(rep_root_path / src_dir_path):
@@ -101,7 +104,10 @@ def sync_folders(src_root_path: Path, rep_root_path: Path) -> None:
 
     for src_file_path in src_files_path_list:
         if src_file_path in rep_files_path_list:
-            if not files_have_same_hash(Path(src_root_path, src_file_path),Path(rep_root_path, src_file_path)):
+            if not files_have_same_hash(
+                src_root_path / src_file_path, 
+                rep_root_path / src_file_path
+            ):
                 handle_file("update", src_root_path, rep_root_path, s_rel=src_file_path)
         else:
             handle_file("create", src_root_path, rep_root_path, s_rel=src_file_path)
@@ -114,7 +120,8 @@ def sync_folders(src_root_path: Path, rep_root_path: Path) -> None:
         if rep_dir_path not in src_dirs_path_list:
             if not included_in_list_parents(rep_dir_path, src_dirs_path_list):
                 handle_dir('remove', rep_root_path, rep_dir_path)   
-  
+
+
 def run_sync(src_path: Path, rep_path: Path, sync_interval: int) -> None:
     logging.info(f"***Initial info***")
     logging.info(f'Choosed source path:   {src_path}')
@@ -122,7 +129,9 @@ def run_sync(src_path: Path, rep_path: Path, sync_interval: int) -> None:
     logging.info(f"Choosed sync interval: {sync_interval}s")
     logging.info(f"***Initial info***")
 
-    schedule.every(sync_interval).seconds.do(sync_folders, src_path, rep_path)
+    schedule.every(sync_interval).seconds.do(
+        sync_folders, src_path, rep_path
+    )
 
     while True:
         schedule.run_pending()
@@ -131,14 +140,18 @@ def run_sync(src_path: Path, rep_path: Path, sync_interval: int) -> None:
 def config_logging(log_path: Path) -> None:
     log_name = datetime.now().strftime("%Y_%m_%d_%H%M%S.log")
     log_path = Path(args.log_dir_path, log_name)
-    #todo - string and dateformat to single variables
+    #todo - string and dateformat to single variables. messy
     logging.basicConfig(
         filename=log_path, 
         level=logging.INFO, 
         format='%(asctime)s.%(msecs)-4d || %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S')
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
     
-    log_formatter = logging.Formatter('%(asctime)s.%(msecs)-04d || %(message)s', '%Y-%m-%d %H:%M:%S')
+    log_formatter = logging.Formatter(
+        '%(asctime)s.%(msecs)-04d || %(message)s', 
+        '%Y-%m-%d %H:%M:%S'
+    )
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(log_formatter)
     logging.getLogger().addHandler(console_handler)
@@ -146,7 +159,8 @@ def config_logging(log_path: Path) -> None:
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="Sync script",
-        description="Program synchronizes files and directories of source folder into replica folder.",
+        description="Program synchronizes files \n"
+                    "and directories of source folder into replica folder.",
         epilog="Happy syncing."
     )
     parser.add_argument(
@@ -162,11 +176,11 @@ def get_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "log_dir_path",
         type=get_valid_abs_path,
-        help="must be a valid directory"
+        help="must be a valid directory, different than src and rep"
     )
     parser.add_argument(
         "sync_interval",
-        type=get_valid_integer,
+        type=get_valid_interval,
         help="must be integer, greater than 0"
     )
     return parser
@@ -176,6 +190,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     config_logging(args.log_dir_path)
 
+    #change to decorate run_sync function
     try:
         run_sync(args.src_root_path, args.rep_root_path, args.sync_interval)
     except KeyboardInterrupt:
