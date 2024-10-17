28aff8d871c72238b8c4961e6da09b55349e4533
michalmoninec <57136223+michalmoninec@users.noreply.github.com>
2024-10-17 11:42:49 +0200

    comparison of files by chunks

--- main.py
+++ main.py
@@ -15,14 +15,22 @@ from file_operations import (
 )
 
 
-def files_have_same_hash(fl1_path: Path, fl2_path: Path) -> bool:
-    with open(fl1_path, "rb") as fl1:
-        with open(fl2_path, "rb") as fl2:
-            hash_fl1 = hashlib.md5(fl1.read()).hexdigest()
-            hash_fl2 = hashlib.md5(fl2.read()).hexdigest()
-            if hash_fl1 == hash_fl2:
+def files_have_same_hash(fl1_path: Path, fl2_path: Path, chunk_size: int) -> bool:
+    with open(fl1_path, "rb") as fl1, open(fl2_path, "rb") as fl2:
+        while True:
+            chunk1 = fl1.read(chunk_size)
+            chunk2 = fl2.read(chunk_size)
+
+            if not chunk1 and not chunk2:
                 return True
-            else:
+
+            if not chunk1 or not chunk2:
+                return False
+
+            hash_fl1 = hashlib.md5(fl1.read(chunk_size)).hexdigest()
+            hash_fl2 = hashlib.md5(fl2.read(chunk_size)).hexdigest()
+
+            if hash_fl1 != hash_fl2:
                 return False
 
 
@@ -44,11 +52,11 @@ def remove_directories(src_dir: Path, rep_dir: Path) -> None:
         remove_dir(path)
 
 
-def create_files(src_dir: Path, rep_dir: Path) -> None:
+def create_files(src_dir: Path, rep_dir: Path, chunk_size: int) -> None:
     for path in src_dir.glob("**/*.*"):
         p_rel = rep_dir / path.relative_to(src_dir)
         if p_rel.exists():
-            if not files_have_same_hash(path, p_rel):
+            if not files_have_same_hash(path, p_rel, chunk_size):
                 update_file(path, p_rel)
         else:
             create_file(path, p_rel)
@@ -61,9 +69,9 @@ def remove_files(src_dir: Path, rep_dir: Path) -> None:
             remove_file(path)
 
 
-def sync_folders(src_root_path: Path, rep_root_path: Path) -> None:
+def sync_folders(src_root_path: Path, rep_root_path: Path, chunk_size: int) -> None:
     create_directories(src_root_path, rep_root_path)
-    create_files(src_root_path, rep_root_path)
+    create_files(src_root_path, rep_root_path, chunk_size)
     remove_files(src_root_path, rep_root_path)
     remove_directories(src_root_path, rep_root_path)
 
@@ -81,15 +89,17 @@ def keyboard_interrupt_handle(func):
 
 
 @keyboard_interrupt_handle
-def run_sync(src_path: Path, rep_path: Path, sync_interval: int) -> None:
+def run_sync(
+    src_path: Path, rep_path: Path, sync_interval: int, chunk_size: int
+) -> None:
     logging.info(f"***Initial info***")
     logging.info(f"Choosed source path:   {src_path}")
     logging.info(f"Choosed replica path:  {rep_path}")
     logging.info(f"Choosed sync interval: {sync_interval}s")
     logging.info(f"***Initial info***")
-    schedule.every(sync_interval).seconds.do(sync_folders, src_path, rep_path)
-
-    # sync_folders(src_path, rep_path)
+    schedule.every(sync_interval).seconds.do(
+        sync_folders, src_path, rep_path, chunk_size
+    )
 
     while True:
         schedule.run_pending()
@@ -132,7 +142,15 @@ def get_parser() -> argparse.ArgumentParser:
         help="must be a valid directory, different than src and rep",
     )
     parser.add_argument(
-        "sync_interval", type=get_valid_interval, help="must be integer, greater than 0"
+        "sync_interval",
+        type=lambda arg: get_valid_positive_integer(arg, "Sync interval"),
+        help="must be integer, greater than 0",
+    )
+    parser.add_argument(
+        "chunk_size",
+        type=lambda arg: get_valid_positive_integer(arg, "Chunk size"),
+        default=1024,
+        help="chunk size of files when compared, must be greater than zero",
     )
     return parser
 
@@ -147,15 +165,18 @@ def get_valid_abs_path(argument: str) -> Path:
         )
 
 
-def get_valid_interval(argument: str) -> int:
-    if int(argument) > 0:
-        return int(argument)
+def get_valid_positive_integer(arg: str, context: str) -> int:
+    if int(arg) > 0:
+        return int(arg)
     else:
-        raise argparse.ArgumentTypeError("Sync interval is not greater than 0")
+        raise argparse.ArgumentTypeError(f"{context} must be greater than 0")
 
 
 def validate_args(
-    parser: argparse.ArgumentParser, src: Path, rep: Path, log: Path
+    parser: argparse.ArgumentParser,
+    src: Path,
+    rep: Path,
+    log: Path,
 ) -> None:
 
     if log == src or log == rep:
@@ -168,8 +189,16 @@ def validate_args(
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
-    validate_args(parser, args.src_root_path, args.rep_root_path, args.log_dir_path)
+    validate_args(
+        parser,
+        args.src_root_path,
+        args.rep_root_path,
+        args.log_dir_path,
+        args.chunk_size,
+    )
 
     config_logging(args.log_dir_path)
 
-    run_sync(args.src_root_path, args.rep_root_path, args.sync_interval)
+    run_sync(
+        args.src_root_path, args.rep_root_path, args.sync_interval, args.chunk_size
+    )
--- test_main.py
+++ test_main.py
@@ -1,37 +1,40 @@
-from main import sync_folders, get_parser, validate_args
-from pathlib import Path
 import pytest
+import argparse
+
+from pathlib import Path
+
+from main import sync_folders, get_parser, validate_args
 from main import files_have_same_hash
 
 
 def test_new_dir(new_temp_dir, empty_playground):
-    sync_folders(empty_playground["src"], empty_playground["rep"])
+    sync_folders(empty_playground["src"], empty_playground["rep"], None)
     assert new_temp_dir.exists()
 
 
 def test_new_file(new_temp_file, empty_playground):
-    sync_folders(empty_playground["src"], empty_playground["rep"])
+    sync_folders(empty_playground["src"], empty_playground["rep"], None)
     assert new_temp_file.exists()
 
 
 def test_spare_file(spare_temp_file, empty_playground):
-    sync_folders(empty_playground["src"], empty_playground["rep"])
+    sync_folders(empty_playground["src"], empty_playground["rep"], None)
     assert not spare_temp_file.exists()
 
 
 def test_spare_dir(spare_temp_dir, empty_playground):
-    sync_folders(empty_playground["src"], empty_playground["rep"])
+    sync_folders(empty_playground["src"], empty_playground["rep"], None)
     assert not spare_temp_dir.exists()
 
 
 def test_changed_file(different_file, empty_playground):
-    sync_folders(empty_playground["src"], empty_playground["rep"])
+    sync_folders(empty_playground["src"], empty_playground["rep"], None)
     fl1 = different_file["fl1"]
     fl2 = different_file["fl2"]
-    assert files_have_same_hash(fl1, fl2)
+    assert files_have_same_hash(fl1, fl2, chunk_size=1024)
 
 
-def test_argparser(empty_playground):
+def test_argparser_raised_exceptions(empty_playground):
     src = str(empty_playground["src"])
     rep = str(empty_playground["rep"])
     log = str(empty_playground["log"])
@@ -60,7 +63,25 @@ def test_argparser(empty_playground):
         parser.parse_args([src, rep, log, ""])
 
     with pytest.raises(SystemExit):
+        parser.parse_args([src, rep, log, "5", "-1"])
+
+    with pytest.raises(SystemExit):
         validate_args(parser, Path(src), Path(rep), Path(src))
 
     with pytest.raises(SystemExit):
         validate_args(parser, Path(src), Path(rep), Path(rep))
+
+
+def test_valid_args(empty_playground):
+    src = str(empty_playground["src"])
+    rep = str(empty_playground["rep"])
+    log = str(empty_playground["log"])
+    parser = get_parser()
+
+    args = parser.parse_args([src, rep, log, "5", "2048"])
+    assert (
+        args.src_root_path == src
+        and args.rep_root_path == rep
+        and args.log_dir_path == log
+        and args.chunk_size == 2048,
+    )
